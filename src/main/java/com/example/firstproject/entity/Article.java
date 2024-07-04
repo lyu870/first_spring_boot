04e75d747beeac9ba98d6d4cfc8cc22aeb69360a
lyu <12duddns@gmail.com>
2024-07-04 18:01:08 +0900

    add Entity and Repository

---
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,33 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+    public Article(Long id, String content, String title) {
+        this.id = id;
+        this.content = content;
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
+
