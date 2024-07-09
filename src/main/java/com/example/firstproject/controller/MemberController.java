6cc979b3ee2641f2cebc32d40b7e99e029469fba
DESKTOP-5IUVORJ\dieb <dieb870@gmail.com>
2024-07-09 22:09:45 +0900

    add lombok code

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class MemberController {
 
@@ -21,11 +23,11 @@ public class MemberController {
 
     @PostMapping("/join")
     public String join(MemberForm memberForm) {
-        System.out.println(memberForm.toString());
+        log.info(memberForm.toString());
         Member member = memberForm.toEntity();
-        System.out.println(member.toString());
+        log.info(member.toString());
         Member saved = memberRepository.save(member);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 @Entity
 public class Article {
     @Id
@@ -16,20 +20,5 @@ public class Article {
 
     @Column
     private String content;
-
-    public Article(Long id, String content, String title) {
-        this.id = id;
-        this.content = content;
-        this.title = title;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
 
