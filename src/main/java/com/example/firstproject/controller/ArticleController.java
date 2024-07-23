d07669b9d33bf45da31489f9c768287a304f46dc
lyu <12duddns@gmail.com>
2024-07-23 17:51:59 +0900

    add index page

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.text.AttributedString;
+import java.util.List;
+
 @Slf4j
 @Controller
 public class ArticleController {
@@ -42,12 +45,29 @@ public class ArticleController {
 
     @GetMapping("/articles/{id}")
     public String show(@PathVariable Long id, Model model) {
+        // ↑ PathVariable은 URL요청으로 들어온 전달값을 컨트롤러의 매개변수로 가져오는 어노테이션
         log.info("id = " + id);
 
+        // 1. id를 조회해 데이터 가져오기
         Article articleEntity = articleRepository.findById(id).orElse(null);
 
+        // 2. 모델에 데이터 등록하기
         model.addAttribute("article", articleEntity);
 
+        // 3. 뷰 페이지 반환하기
         return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+
+        // 1. 모든 데이터 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+
+        // 3. 뷰 페이지 반환하기
+        return "articles/index";
     }
 }
--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -1,13 +1,18 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.entity.Article;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -29,5 +34,18 @@ public class MemberController {
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
         return "";
+    }
+
+    public String show(@PathVariable Long id, Model model) {
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", member);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        Iterable<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "members/index";
     }
 }
